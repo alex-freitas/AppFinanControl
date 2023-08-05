19634f0e1d5ab1415799c812ee15288b4c6e781d
Alex Freitas <alex.abreu.freitas@gmail.com>
2023-08-05 11:47:59 +0200

    add app icons

--- AppFinanControl/MainPage.xaml.cs
+++ AppFinanControl/MainPage.xaml.cs
@@ -2,24 +2,21 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    int count = 0;
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+        CounterBtn.Text = $"Clicked {count} time" + (count == 1 ? "" : "s");
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
 }
 
 
