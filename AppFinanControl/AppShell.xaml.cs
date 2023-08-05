daf8606316ab12fae2c8fb5b34f63b7b8a768ab2
De Abreu Freitas, Alex <Alex.Deabreufreitas@jsmd-group.com>
2023-08-05 10:22:12 +0200

    first commit

---
+++ AppFinanControl/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppFinanControl;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ AppFinanControl/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AppFinanControl;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
